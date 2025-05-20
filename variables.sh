68376d179ebc8968f18cf4e8f79c36abd0b1ba66
Deenakumari <deenamadhumanchi@gmail.com>
2025-05-20 17:03:03 +0530

    hello-world

---
+++ variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Deena
+PERSON2=Naga
+
+echo "$PERSON1: Hey $PERSON2, how are you ?"
+echo "$PERSON2: Hello $PERSON1, i am fine hwhat about you"
+echo "$PERSON1: I am also fine. what's up?"
+echo "$PERSON2: nothing, just going to mars now, will you come"
+echo "$PERSON1: sorry, you carry on! i will come once you come back"
