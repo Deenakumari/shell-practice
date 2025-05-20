0c2c1190882b706de80ce04cddc1bf3d4e297cf7
Deenakumari <deenamadhumanchi@gmail.com>
2025-05-20 16:55:31 +0530

    hello-world

---
+++ conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Deena: Hey naga, how are you ?"
+echo "Naga: Hello deena, i am fine hwhat about you"
+echo "Deena: I am also fine. what's up?"
+echo "Naga: nothing, just going to mars now, will you come"
+echo "Deena: sorry, you carry on! i will come once you come back"
